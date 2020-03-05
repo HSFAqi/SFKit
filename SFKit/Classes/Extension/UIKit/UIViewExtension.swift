1c90b0f5192273fcd70bb99d9db5e33da340a779
hsf <hsf_ios@sina.com>
2020-03-05 20:57:40 +0800

    给UIView添加扩展

---
+++ SFKit/Classes/Extension/UIKit/UIViewExtension.swift
@@ -0,0 +1,235 @@
+//
+//  UIViewExtension.swift
+//  SwiftTestDemo
+//
+//  Created by 黄山锋 on 2020/3/5.
+//  Copyright © 2020 SFTeam. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - 圆角与阴影
+public extension UIView {
+    /// 边框颜色
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+        set {
+            guard let color = newValue else {
+                layer.borderColor = nil
+                return
+            }
+            // Fix React-Native conflict issue
+            guard String(describing: type(of: color)) != "__NSCFType" else { return }
+            layer.borderColor = color.cgColor
+        }
+    }
+    
+    /// 边框宽度
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    /// 圆角半径
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.masksToBounds = true
+            layer.cornerRadius = abs(CGFloat(Int(newValue * 100)) / 100)
+        }
+    }
+    
+    /// 阴影颜色
+    @IBInspectable var shadowColor: UIColor? {
+        get {
+            guard let color = layer.shadowColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+        set {
+            layer.shadowColor = newValue?.cgColor
+        }
+    }
+    
+    /// 阴影偏移量
+    @IBInspectable var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+
+    /// 阴影不透明度
+    @IBInspectable var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+
+    /// 阴影半径
+    @IBInspectable var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+    
+    /// 设置阴影
+    /// - Parameters:
+    ///   - color: 阴影颜色
+    ///   - opacity: 不透明度
+    ///   - radius: 半径
+    ///   - offset: 偏移
+    ///
+    /// 使用shadowPath，避免离屏渲染。
+    func setShadow(color: UIColor, opacity: Float, radius: CGFloat, offset: CGSize) {
+        layer.masksToBounds = false
+        clipsToBounds = false
+        layer.shadowColor = color.cgColor
+        layer.shadowOpacity = opacity
+        layer.shadowRadius = radius
+        layer.shadowOffset = offset
+        let path = UIBezierPath(rect: bounds)
+        layer.shadowPath = path.cgPath
+    }
+    
+    /// 设置圆角
+    /// - Parameter radius: 圆角半径
+    ///
+    /// UIView(不包括子类)、UILabel、UITextField、UITextView，UIButton，直接使用layer.cornerRadius 一行代码就行。
+    /// 为避免离屏渲染，请合理使用：layer.masksToBounds = true 和 clipsToBounds = true
+    func setCorner(radius: CGFloat) {
+        layer.cornerRadius = radius
+    }
+}
+
+// MARK: - frame
+public extension UIView {
+    /// x
+    var x: CGFloat {
+        get { return frame.origin.x }
+        set(newValue) {
+            var tempFrame: CGRect = frame
+            tempFrame.origin.x = newValue
+            frame = tempFrame
+        }
+    }
+
+    /// y
+    var y: CGFloat {
+        get { return frame.origin.y }
+        set(newValue) {
+            var tempFrame: CGRect = frame
+            tempFrame.origin.y = newValue
+            frame = tempFrame
+        }
+    }
+
+    /// height
+    var height: CGFloat {
+        get { return frame.size.height }
+        set(newValue) {
+            var tempFrame: CGRect = frame
+            tempFrame.size.height = newValue
+            frame = tempFrame
+        }
+    }
+
+    /// width
+    var width: CGFloat {
+        get { return frame.size.width }
+        set(newValue) {
+            var tempFrame: CGRect = frame
+            tempFrame.size.width = newValue
+            frame = tempFrame
+        }
+    }
+
+    /// size
+    var size: CGSize {
+        get { return frame.size }
+        set(newValue) {
+            var tempFrame: CGRect = frame
+            tempFrame.size = newValue
+            frame = tempFrame
+        }
+    }
+    
+    /// centerX
+    var centerX: CGFloat {
+        get { return center.x }
+        set(newValue) {
+            var tempCenter: CGPoint = center
+            tempCenter.x = newValue
+            center = tempCenter
+        }
+    }
+
+    /// centerY
+    var centerY: CGFloat {
+        get { return center.y }
+        set(newValue) {
+            var tempCenter: CGPoint = center
+            tempCenter.y = newValue
+            center = tempCenter;
+        }
+    }
+}
+
+// MARK: - Others
+public extension UIView {
+    /// 从Nib中加载
+    class func loadFromNib(named name: String, bundle: Bundle? = nil) -> UIView? {
+        return UINib(nibName: name, bundle: bundle).instantiate(withOwner: nil, options: nil)[0] as? UIView
+    }
+    
+    /// 添加子视图（数组）
+    func addSubviews(_ subviews: [UIView]) {
+        subviews.forEach { addSubview($0) }
+    }
+    
+    /// 移除子视图（数组）
+    func removeSubviews() {
+        subviews.forEach({ $0.removeFromSuperview() })
+    }
+    
+    /// 递归查找，直到查找到父视图中符合predicate的某个View
+    func ancestorView(where predicate: (UIView?) -> Bool) -> UIView? {
+        if predicate(superview) {
+            return superview
+        }
+        return superview?.ancestorView(where: predicate)
+    }
+
+    /// 递归查找，直到查找到父视图中符合withClass类型的某个View
+    func ancestorView<T: UIView>(withClass name: T.Type) -> T? {
+        return ancestorView(where: { $0 is T }) as? T
+    }
+    
+    /// 截屏
+    var screenshot: UIImage? {
+        UIGraphicsBeginImageContextWithOptions(layer.frame.size, isOpaque, 0)
+        defer {
+            UIGraphicsEndImageContext()
+        }
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        layer.render(in: context)
+        return UIGraphicsGetImageFromCurrentImageContext()
+    }
+}
